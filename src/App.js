8ebd4515e8fa1f9c1fcf3c7e414754ac55e6e5b8
ahangel256@gmail.com <ahangel256@gmail.com>
2024-04-05 20:57:05 -0600

    Navegación

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import CartContent from './components/CartContent/CartContent';
 import Home from './components/Home/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <Home/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/cart' element={<CartContent/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
