82e918ad78732583391e699bf1bfcbf5d449430b
ahangel256@gmail.com <ahangel256@gmail.com>
2024-04-07 16:45:54 -0600

    Cart

--- src/components/CartContent/CartElements.js
+++ src/components/CartContent/CartElements.js
@@ -6,14 +6,26 @@ import "./CartContent.css"
 import React from 'react'
 
 const CartElements = () => {
-    const { cart } = useContext(dataContext)
+    const { cart, setCart } = useContext(dataContext)
+
+    const deleteProduct = (id) => {
+        const foundId = cart.find((element) => element.id === id)
+
+        const newCart = cart.filter((element) => {
+            return element !== foundId
+        })
+
+        setCart(newCart)
+    }
+
     return cart.map((product) => {
         return (
             <div className="cartContent" key={product.id}>
                 <img src={product.img} alt="product-card"></img>
                 <h3 className="name">{product.name}</h3>
-                <CartItemCounter product={product}/>
+                <CartItemCounter product={product} />
                 <h4 className="price">${product.price * product.quantity}</h4>
+                <h3 className="cart-delete-button" onClick={() => deleteProduct(product.id)}>❌</h3>
             </div>
         )
     })
