e5e62632caf1d7953d904de44e10ea33dcc0d1f7
ahangel256@gmail.com <ahangel256@gmail.com>
2024-04-08 19:33:38 -0600

    Added localStorage

--- src/components/Context/DataContext.js
+++ src/components/Context/DataContext.js
@@ -1,18 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const dataContext = createContext();
 
 const DataProvider = ({ children }) => {
+
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || []
+    const [cart, setCart] = useState(savedCart);
+
     
-    const [cart, setCart] = useState([]);
+    useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart))
+    }, [cart])
 
     const buyProduct = (product) => {
         const productRepeated = cart.find((item) => item.id === product.id)
 
         if (productRepeated) {
-            setCart(cart.map((item) => item.id === product.id 
-            ? { ...product, quantity: product.quantity + 1 } 
-            : item))
+            setCart(cart.map((item) => item.id === product.id
+                ? { ...product, quantity: product.quantity + 1 }
+                : item))
         } else {
             setCart([...cart, product])
         }
